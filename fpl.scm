9026343324e20dd3dd8e7b5dcf11500ecd2ccb30
Aarya BC <aaryabc49@gmail.com>
2019-02-09 14:03:25 -0500

    Fixed extra +0 that was unnecessary

--- fpl.scm
+++ fpl.scm
@@ -62,7 +62,7 @@ conditions:
 |#
 (define (sum-up-numbers-simple L)
   (cond ((null? L) 0)
-        ( (number? (car L)) (+ 0 (car L) (sum-up-numbers-simple (cdr L))))
+        ( (number? (car L)) (+ (car L) (sum-up-numbers-simple (cdr L))))
         ( (sum-up-numbers-simple (cdr L)) )
   )
 )
